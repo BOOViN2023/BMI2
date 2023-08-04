464ec89170e81a498c8a8a4473e17015fc467011
Boovin M <141322515+BOOViN2023@users.noreply.github.com>
2023-08-04 15:09:54 +0530

    Create bmi.js

---
+++ bmi.js
@@ -0,0 +1,43 @@
+let height, weight,measure, error, bmi;
+function calculate()
+{
+
+    height=document.getElementById("height").value;
+    weight=document.getElementById("weight").value;
+    error= "Given Values Are Wrong";
+    height/=100;
+    height*=height;
+    bmi=weight/height;
+    bmi=bmi.toFixed(1);
+
+    if (bmi <= 18.4) 
+    {
+        measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
+    } 
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+        measure = "Your BMI is " + bmi + " which means " + "You are Normal";
+    } 
+    else if (bmi >= 25 && bmi <= 29.9) {
+        measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
+    } 
+    else if (bmi >= 30) {
+        measure = "Your BMI is " + bmi + " which means " + "You are Obese";
+    }
+
+    if(weight<=0 || height<=0)
+    {
+        document.getElementById("results").innerHTML=error;
+    }
+    else if(height<50 || height>220)
+    {
+        document.getElementById("results").innerHTML=error;
+    }
+    else if(weight<10)
+    {
+        document.getElementById("results").innerHTML=error;
+    }
+    else
+    {
+        document.getElementById("results").innerHTML=measure;
+    }
+}
